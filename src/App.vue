59dc752e6eec0df99f11f550fec998b595cac863
Iraklis Georgas <iraklis.g@gmail.com>
2021-05-14 12:00:21 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <form>
+    <FormSection>
+      <input type="text" v-model="inputs.foo">
+    </FormSection>
+    <input type="text" v-model="inputs.bar">
+  </form>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from 'vue'
+import FormSection from "./components/FormSection.vue";
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    FormSection
+  },
+  setup() {
+    const inputs = ref({
+      foo: '',
+      bar: '',
+    });
+
+    return { inputs }
+  }
+})
+</script>
---
+++ src/components/FormSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <fieldset>
+    <legend>My legend</legend>
+    <slot></slot>
+  </fieldset>
+</template>
+
+<script>
+export default {
+  name: 'FormSection',
+};
+</script>
+
+<style scoped>
+
+</style>
